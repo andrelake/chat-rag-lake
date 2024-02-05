b7b46e24b19afd05fd2c86dc326fca16221e0bd1
Felipe Augusto Arantes de Souza <felipmoreira8@gmail.com>
2024-02-05 11:52:29 -0300

    Fix imports 2

--- db_conn.py
+++ db_conn.py
@@ -1,10 +1,11 @@
-from langchain_community.vectorstores import AstraDB
+from langchain_community.vectorstores import AstraDB as LangchainAstraDB
+from astrapy.db import AstraDB as AstrapyAstraDB
 from env import OPENAI_API_KEY, ASTRA_DB_API_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN
 
 from langchain_openai import OpenAIEmbeddings
 
 
-db = AstraDB(
+db = AstrapyAstraDB(
     token=ASTRA_DB_APPLICATION_TOKEN,
     api_endpoint=ASTRA_DB_API_ENDPOINT,
     namespace="default_keyspace"
@@ -12,7 +13,7 @@ db = AstraDB(
 
 embedding = OpenAIEmbeddings()
 
-coll_movies_db = AstraDB(
+coll_movies_db = LangchainAstraDB(
     embedding=embedding,
     collection_name="movies_db",
     token=ASTRA_DB_APPLICATION_TOKEN,
