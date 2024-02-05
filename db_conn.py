e96cb5b00bf0a8efe9dd583f2096862807293480
Felipe Augusto Arantes de Souza <felipmoreira8@gmail.com>
2024-02-05 13:55:16 -0300

    Import & langchain fixes

--- db_conn.py
+++ db_conn.py
@@ -1,7 +1,6 @@
 import json
 
-from langchain_community.vectorstores import AstraDB as LangchainAstraDB
-from astrapy.db import AstraDB as AstraDB, AstraDBCollection
+from astrapy.db import AstraDB, AstraDBCollection
 from env import OPENAI_API_KEY, ASTRA_DB_API_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN
 
 from langchain_openai import OpenAIEmbeddings
@@ -23,10 +22,17 @@ db = connect(
     astra_db_api_endpoint=ASTRA_DB_API_ENDPOINT,
 )
 
-# embedding = OpenAIEmbeddings()
-# coll_movies_db = LangchainAstraDB(
-#     embedding=embedding,
-#     collection_name="movies_db",
-#     token=ASTRA_DB_APPLICATION_TOKEN,
-#     api_endpoint=ASTRA_DB_API_ENDPOINT,
-# )
+
+# Not working
+def connect_with_langchain_openai(astra_db_application_token: str, astra_db_api_endpoint: str, collection_name: str):
+    from langchain_community.vectorstores import AstraDB as LangchainAstraDB
+
+
+    embedding = OpenAIEmbeddings()
+    coll_movies_db = LangchainAstraDB(
+        embedding=embedding,
+        collection_name=collection_name,
+        token=astra_db_application_token,
+        api_endpoint=astra_db_api_endpoint,
+    )
+    return coll_movies_db
--- integration.py
+++ integration.py
@@ -1,5 +1,4 @@
 import os
-from langchain.schema import Document
 
 from db_conn import db, import_documents
 
