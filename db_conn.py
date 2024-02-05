65e1df4d944882ec0c942fb007b7b1fdf1054ea4
Felipe Augusto Arantes de Souza <felipmoreira8@gmail.com>
2024-02-05 13:46:30 -0300

    Added db methods

--- db_conn.py
+++ db_conn.py
@@ -1,21 +1,32 @@
+import json
+
 from langchain_community.vectorstores import AstraDB as LangchainAstraDB
-from astrapy.db import AstraDB as AstrapyAstraDB
+from astrapy.db import AstraDB as AstraDB, AstraDBCollection
 from env import OPENAI_API_KEY, ASTRA_DB_API_ENDPOINT, ASTRA_DB_APPLICATION_TOKEN
 
 from langchain_openai import OpenAIEmbeddings
 
+def connect(astra_db_application_token: str, astra_db_api_endpoint: str, namespace: str = 'default_keyspace'):
+    return AstraDB(
+        token=astra_db_application_token,
+        api_endpoint=astra_db_api_endpoint,
+        namespace=namespace
+    )
 
-db = AstrapyAstraDB(
-    token=ASTRA_DB_APPLICATION_TOKEN,
-    api_endpoint=ASTRA_DB_API_ENDPOINT,
-    namespace="default_keyspace"
+def import_documents(collection: AstraDBCollection, json_path: str):
+    with open(json_path, 'r') as file:
+        documents = json.load(file)
+    collection.insert_many(documents)
+
+db = connect(
+    astra_db_application_token=ASTRA_DB_APPLICATION_TOKEN,
+    astra_db_api_endpoint=ASTRA_DB_API_ENDPOINT,
 )
 
-embedding = OpenAIEmbeddings()
-
-coll_movies_db = LangchainAstraDB(
-    embedding=embedding,
-    collection_name="movies_db",
-    token=ASTRA_DB_APPLICATION_TOKEN,
-    api_endpoint=ASTRA_DB_API_ENDPOINT,
-)
+# embedding = OpenAIEmbeddings()
+# coll_movies_db = LangchainAstraDB(
+#     embedding=embedding,
+#     collection_name="movies_db",
+#     token=ASTRA_DB_APPLICATION_TOKEN,
+#     api_endpoint=ASTRA_DB_API_ENDPOINT,
+# )
