53e80965bcf499bbd3b4ac0e8e4d1020c6a39d98
andreLake <andre.lake@hotmail.com>
2024-02-18 19:56:48 -0300

    QA working

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from time import perf_counter
-
-from data_handler import load_document, chunk_data, insert_or_fetch_embeddings
-
+from time import perf_counter, sleep
 from env import PINECONE_INDEX_NAME
-# from openai_interaction import get_interaction
 
-filepath = "data/constituicao_federal.pdf"
+from data_handler import load_document, chunk_data, insert_or_fetch_embeddings, delete_pinecone_index
+from openai_interaction import asking_and_getting_answers
+
+
+filepath = "data/Top 50 Java Interveiw Questions and Answers.pdf"
 
 
 def main() -> None:
     data = load_document(filepath)
     chunks = chunk_data(data)
-    insert_or_fetch_embeddings(PINECONE_INDEX_NAME, chunks)
-    # get_interaction(OPENAI_API_KEY)
+    vector_store = insert_or_fetch_embeddings(PINECONE_INDEX_NAME, chunks)
+
+    i = 1
+    print("Digite Sair ou Fim para encerrar.")
+    while True:
+        question = input(f"\nPergunta #{i}: ")
+        if question.lower() in ["sair", "fim"]:
+            print("Tchau!")
+            sleep(2)
+            break
+
+        answer = asking_and_getting_answers(vector_store, question)
+        print(f"\nResposta : {answer}")
+        print(f"\n {'-' * 50}")
+        i += 1
 
 
 if __name__ == "__main__":
+    print("\nStarting...")
     time_start = perf_counter()
     main()
     time_end = perf_counter()
--- openai_interaction.py
+++ openai_interaction.py
@@ -1,17 +1,14 @@
 from langchain.agents import AgentExecutor
-
-from env import OPENAI_MODEL_NAME
-
-from langchain.prompts import ChatPromptTemplate, HumanMessagePromptTemplate
-from langchain_core.messages import SystemMessage
-from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-
-from langchain.globals import set_llm_cache
 from langchain.cache import InMemoryCache
-
+from langchain.chains import LLMChain
+from langchain.globals import set_llm_cache
+from langchain.prompts import (ChatPromptTemplate, HumanMessagePromptTemplate)
+from langchain_core.messages import SystemMessage
 from langchain_experimental.agents.agent_toolkits import create_python_agent
 from langchain_experimental.tools.python.tool import PythonREPLTool
+from langchain_openai import ChatOpenAI
+
+from env import OPENAI_API_KEY, OPENAI_MODEL_NAME
 
 
 def get_interaction(key: str) -> None:
@@ -45,3 +42,21 @@ def get_interaction(key: str) -> None:
     resp = agent_executor.invoke({"input": instruction})
     # resp = chain.invoke({"instruction": instruction})
     print(agent_executor.tools[0].name, resp.get('output'))
+
+
+def asking_and_getting_answers(vectorstore, question: str):
+    from langchain.chains import RetrievalQA
+
+    # llm
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL_NAME, temperature=0.5)
+
+    # retriever
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+    # retrieval qa
+    qa_chain = RetrievalQA.from_chain_type(
+        llm=llm, chain_type="stuff", retriever=retriever, verbose=True
+    )
+
+    resp = qa_chain.invoke({"query": question})
+    return resp['result']
