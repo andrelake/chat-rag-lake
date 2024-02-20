1ba07076798fadc7a0c4598025ff7ecc79c2e3ad
Felipe Augusto Arantes de Souza <felipmoreira8@gmail.com>
2024-02-19 22:51:16 -0300

    Minor visual changes (Python PEPs)

--- data_handler.py
+++ data_handler.py
@@ -9,17 +9,15 @@ import tiktoken
 
 
 def load_document(filepath) -> list:
-    print(f"Loading {filepath}")
-
+    print(f'Loading {filepath}')
     loader = PyPDFLoader(filepath)
     data = loader.load()
-
-    print(f"\nData Loaded Successfully. Total pages: {len(data)}")
+    print(f'\nData Loaded Successfully. Total pages: {len(data)}')
     return data
 
 
 def chunk_data(data: list, chunk_size=1600) -> list:
-    print(f"\nStarting chunk context")
+    print(f'\nStarting chunk context')
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=chunk_size,
         chunk_overlap=0
@@ -27,65 +25,57 @@ def chunk_data(data: list, chunk_size=1600) -> list:
     chunks = text_splitter.split_documents(data)
     i = 1
     for chunk in chunks:
-        print(f"#{i} {chunk.page_content}\n")
+        print(f'#{i} {chunk.page_content}\n')
         i += 1
-
-    print(f"\nTotal chunks: {len(chunks)}")
+    print(f'\nTotal chunks: {len(chunks)}')
     return chunks
 
 
 def insert_or_fetch_embeddings(index_name: str, chunks: list):
-
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-
-    pinecone = Pinecone(
-        api_key=PINECONE_API_KEY
-    )
-
+    pinecone = Pinecone(api_key=PINECONE_API_KEY)
     indexes = pinecone.list_indexes()
-    print(f"\nIndexes: {len(indexes)}")
+    print(f'\nIndexes: {len(indexes)}')
 
     if len(indexes) == 0:
         return create_vector_store(chunks, embeddings, index_name, pinecone)
     else:
         for index in indexes:
             if index['name'] == index_name:
-                print(f"\nGetting info from index: {index_name}")
+                print(f'\nGetting info from index: {index_name}')
                 return PineconeVectorstore.from_existing_index(index_name, embeddings)
             else:
                 return create_vector_store(chunks, embeddings, index_name, pinecone)
 
 
 def create_vector_store(chunks, embeddings, index_name, pinecone):
-    print(f"\nCreating index: {index_name}")
-    pinecone.create_index(index_name, dimension=1536, metric="cosine", spec=ServerlessSpec(
-        cloud='aws',
-        region=PINECONE_REGION,
-    ))
+    print(f'\nCreating index: {index_name}')
+    pinecone.create_index(
+        index_name,
+        dimension=1536,
+        metric='cosine',
+        spec=ServerlessSpec(cloud='aws', region=PINECONE_REGION)
+    )
     return PineconeVectorstore.from_documents(chunks, embeddings, index_name=index_name)
 
 
 def delete_pinecone_index(index_name='all'):
-
-    pinecone = Pinecone(
-        api_key=PINECONE_API_KEY
-    )
-
+    pinecone = Pinecone(api_key=PINECONE_API_KEY)
     if index_name == 'all':
         for index in pinecone.list_indexes():
-            print(f"\nDeleting all indexes...")
+            print(f'\nDeleting all indexes...')
             pinecone.delete_index(index)
-            print("\nAll Indexes Deleted Successfully.")
+            print('\nAll Indexes Deleted Successfully.')
     else:
-        print(f"\nDeleting index: {index_name}", end="")
+        print(f'\nDeleting index: {index_name}', end='')
         pinecone.delete_index(index_name)
-        print("\nIndex Deleted Successfully.")
+        print('\nIndex Deleted Successfully.')
 
 
 def show_embeddings_cost(texts):
-    print(f"\nStarting embedding price calculation")
-    encoding = tiktoken.encoding_for_model("text-embedding-ada-002")
+    print(f'\nStarting embedding price calculation')
+    encoding = tiktoken.encoding_for_model('text-embedding-ada-002')
     total_tokens = sum([len(encoding.encode(page.page_content)) for page in texts])
-    print(f"Total tokens: {total_tokens}")
-    print(f"Total pages: {len(texts)}")
-    print(f"Embedding cost: ${total_tokens * 0.0004 / 1000:.4f}")
+    print(f'Total tokens: {total_tokens}')
+    print(f'Total pages: {len(texts)}')
+    print(f'Embedding cost: ${total_tokens * 0.0004 / 1000:.4f}')
